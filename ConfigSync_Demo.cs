d4040aa8a01a1be83f67589854161362593a041e
Notest_ <noccqlive@gmail.com>
2024-04-18 05:31:47 -0300

    Add project files.

---
+++ ConfigSync_Demo.cs
@@ -0,0 +1,71 @@
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using ConfigSync;
+using HarmonyLib;
+using System;
+
+namespace ConfigSync_Demo
+{
+    [ContentWarningPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_VERSION, true)]
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInDependency(ConfigSync.MyPluginInfo.PLUGIN_GUID)]
+    public class ConfigSync_Demo : BaseUnityPlugin
+    {
+        internal static ConfigEntry<int>? BepInExConfig;
+        public static ConfigSync_Demo Instance { get; private set; } = null!;
+        internal new static ManualLogSource Logger { get; private set; } = null!;
+        internal static Harmony? Harmony { get; set; }
+        internal static int Value = new Random().Next(1, 20);
+        internal static Configuration? SyncedConfig;
+        internal int ConfigValue;
+
+        private void Awake()
+        {
+            Logger = base.Logger;
+            Instance = this;
+
+            BepInExConfig = Config.Bind("General", "ConfigDemo", Value,
+            "Random number.");
+
+            Configuration SyncedConfig = new Configuration("ConfigDemo", "ConfigSync_ConfigDemo", BepInExConfig.Value);
+            int SyncedConfigValue = (int)SyncedConfig.CurrentValue;
+
+            BepInExConfig.SettingChanged += delegate
+            {
+                SyncedConfig.SetValue(BepInExConfig.Value);
+                Logger.LogWarning("BepInExConfig value changed");
+            };
+
+            SyncedConfig.ConfigChanged += delegate
+            {
+                Logger.LogWarning($"SyncedConfig value changed {(int)SyncedConfig.CurrentValue}");
+                SyncedConfigValue = (int)SyncedConfig.CurrentValue;
+            };
+            
+            Patch();
+
+            Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
+        }
+
+        internal static void Patch()
+        {
+            Harmony ??= new Harmony(MyPluginInfo.PLUGIN_GUID);
+
+            Logger.LogDebug("Patching...");
+
+            Harmony.PatchAll();
+
+            Logger.LogDebug("Finished patching!");
+        }
+
+        internal static void Unpatch()
+        {
+            Logger.LogDebug("Unpatching...");
+
+            Harmony?.UnpatchSelf();
+
+            Logger.LogDebug("Finished unpatching!");
+        }
+    }
+}
